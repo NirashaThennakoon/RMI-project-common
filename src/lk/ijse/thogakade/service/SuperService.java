55d69fbfbf1f4d8e230280b30b2cd485964a36cc
NirashaThennakoon <nmalinthi@gmail.com>
2017-03-07 08:24:14 +0530

    first commit

---
+++ src/lk/ijse/thogakade/service/SuperService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.ijse.thogakade.service;
+
+import java.rmi.Remote;
+import java.util.ArrayList;
+import lk.ijse.thogakade.reservation.Reservation;
+
+/**
+ *
+ * @author student
+ */
+public interface SuperService<T> extends Remote,Reservation {
+    public boolean add(T t) throws Exception;
+
+    public boolean update(T t) throws Exception;
+
+    public boolean delete(T t) throws Exception;
+
+    public T get(String id) throws Exception;
+    
+    
+
+    public ArrayList<T> getAll() throws Exception;
+}
